ac407516492beb8d16e1ca62050d1cc31bf41c43
고은아 <kona@mpaycheck.com>
2020-01-20 15:00:38 +0900

    TIL 20-01-20 finish

---
+++ vue-form/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <!-- 이벤트 기본 새로고침을 막겠다 -->
+    <div>
+      <label for="username">id : </label>
+      <input v-model="username" id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw : </label>
+      <input v-model="password" id="password" type="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    submitForm: function() {
+      const url = "http://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      axios
+        .post(url, data)
+        // eslint-disable-next-line no-console
+        .then(res => console.log(res))
+        .catch();
+    }
+  }
+};
+</script>
+
+<style></style>
